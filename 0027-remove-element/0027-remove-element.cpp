d15f4d4116ce639781783c0cf893f72b2ee69db4
vtu28343-Ajay <vtu28343@veltech.edu.in>
2026-08-29 11:28:47 +0530

    Time: 0 ms (100%), Space: 11.6 MB (84.39%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0; // Pointer to place the next valid element
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
